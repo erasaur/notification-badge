891cf768ad56ad1c306df400e8649a62a700b9c5
erasaur <erasaur@gmail.com>
2014-11-19 16:00:34 -0800

    Update info

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'erasaur:notification-badge',
-  summary: ' /* Fill me in! */ ',
-  version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  summary: 'Simple, Animated Notification Badge Template for Meteor',
+  version: '0.0.0',
+  git: 'https://github.com/erasaur/notification-badge'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('erasaur:notification-badge.js');
+  api.addFiles('lib/erasaur:notification-badge.js');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('erasaur:notification-badge');
-  api.addFiles('erasaur:notification-badge-tests.js');
+  api.addFiles('tests/erasaur:notification-badge-tests.js');
 });
