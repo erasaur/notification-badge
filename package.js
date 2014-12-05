6674a9e5112629369e7daba6e6f57d4a1ac5d6e7
Mark Lee <marklee.lhs@gmail.com>
2014-12-05 12:55:26 -0800

    Guard against multiple simultaneous count changes

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'erasaur:notification-badge',
   summary: 'Simple, Animated Notification Badge Template for Meteor',
-  version: '0.0.5',
+  version: '0.0.6',
   git: 'https://github.com/erasaur/notification-badge'
 });
 
